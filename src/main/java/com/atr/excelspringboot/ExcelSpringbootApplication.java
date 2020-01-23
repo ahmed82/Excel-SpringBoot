cfcf79dbbc46170c7c7b234c65e9c2de4f1027ee
1426391 <ahmed.al-salih@delhaize.com>
2020-01-23 11:31:58 -0500

    Excel workbook in spring boot

---
+++ src/main/java/com/atr/excelspringboot/ExcelSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.atr.excelspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atr/excelspringboot/ExcelSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atr.excelspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
